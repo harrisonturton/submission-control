53f405fc1f5594fd337c0f518392b6655a7c851a
Harrison Turton <harrisonturton@gmail.com>
2018-10-17 09:53:10 +1100

    Added String() methods for each type in type/

--- types/types.go
+++ types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "fmt"
+
 // Arguments required to create a service
 type ServiceCreateSpec struct {
 	BaseImage string
@@ -16,4 +18,26 @@ type ServiceRemoveSpec struct {
 type ServiceScaleSpec struct {
 	ServiceID string
 	Replicas  uint64
+}
+
+func (spec *ServiceCreateSpec) String() string {
+	result := ""
+	result += fmt.Sprintf("\n  Base Image: %s", spec.BaseImage)
+	result += fmt.Sprintf("\n  Replicas: %d", spec.Replicas)
+	result += "\n  Comands: "
+	for _, command := range spec.Commands {
+		result += command + " "
+	}
+	return result + "\n"
+}
+
+func (spec *ServiceRemoveSpec) String() string {
+	return fmt.Sprintf("\n  Service ID: %s\n", spec.ServiceID)
+}
+
+func (spec *ServiceScaleSpec) String() string {
+	result := ""
+	result += fmt.Sprintf("\n  Service ID: %s", spec.ServiceID)
+	result += fmt.Sprintf("\n  Replicas: %d\n", spec.Replicas)
+	return result
 }
