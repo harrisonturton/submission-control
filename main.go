90f9ab24529dec70252c7d6a98eb4930339da3ef
Harrison Turton <harrisonturton@gmail.com>
2018-10-16 21:47:42 +1100

    Seperated CLI into separate repo. Removed cli/ & environment/ to clean up.

--- main.go
+++ main.go
@@ -2,40 +2,30 @@ package main
 
 import (
 	"fmt"
-	"github.com/harrisonturton/hydra-ci/cli"
-	"github.com/harrisonturton/hydra-ci/environment"
-	"sync"
+	"github.com/harrisonturton/hydra-daemon/container"
+	"time"
 )
 
-const (
-	EnvironmentCount = 5
-	MessageCount     = 10
-)
-
-var wg sync.WaitGroup
-var requests = make(chan string, MessageCount)
-var results = make(chan string, MessageCount)
-var stop = make(chan bool)
+var replicas uint64 = 3
+var name = "test"
 
 func main() {
-	for i := 0; i < EnvironmentCount; i++ {
-		wg.Add(1)
-		env := environment.NewEnvironment("python-base", requests, results)
-		go env.Run(&wg, stop)
+	client, err := container.NewClient("1.38")
+	panicErr(err)
+
+	resp, err := client.CreateService(name, "alpine", replicas, []string{"ping", "docker.com"})
+	panicErr(err)
+	fmt.Println(fmt.Sprintf("ID: %s", resp.ID))
+
+	time.Sleep(time.Second * 10)
+	err = client.ScaleService(resp.ID, 7)
+	panicErr(err)
+	fmt.Println("Increased number of replicas")
+}
+
+func panicErr(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
 	}
-	wg.Add(2)
-	go cli.Run(stop, &wg, requests)
-	go func() {
-		for {
-			select {
-			case result := <-results:
-				fmt.Println(result)
-			case <-stop:
-				wg.Done()
-				return
-			}
-		}
-	}()
-	wg.Wait()
-	fmt.Println("Goodbye!")
 }
