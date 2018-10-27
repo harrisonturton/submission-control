4aa93ac806406729bbe7929cf2f0684ba9877cc4
Harrison Turton <harrisonturton@gmail.com>
2018-10-27 20:33:23 +1100

    Add tests for worker.Run and worker.handleJob

--- ci/worker/worker/worker_test.go
+++ ci/worker/worker/worker_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/harrisonturton/submission-control/ci/mock/client"
 	"github.com/harrisonturton/submission-control/ci/mock/queue"
 	"os"
+	"sync"
 	"testing"
 )
 
@@ -19,4 +20,43 @@ func TestNew(t *testing.T) {
 	jobQueue, _ := queue.New(jobQueueName, queueAddr)
 	resultQueue, _ := queue.New(resultQueueName, queueAddr)
 	New(jobQueue, resultQueue, client, os.Stdout)
+}
+
+func TestRun(t *testing.T) {
+	// Create worker
+	client, _ := client.New(dockerVersion)
+	jobs, _ := queue.New(jobQueueName, queueAddr)
+	results, _ := queue.New(resultQueueName, queueAddr)
+	worker := New(jobs, results, client, os.Stdout)
+	// Test
+	var wg sync.WaitGroup
+	wg.Add(1)
+	done := make(chan bool)
+	jobs.Message("test")
+	go worker.Run(done, &wg)
+	close(done)
+	wg.Wait()
+	select {
+	case <-results.Messages:
+		break
+	default:
+		t.Errorf("Failed to put data on result queue")
+	}
+}
+
+func TestHandleJob(t *testing.T) {
+	// Create worker
+	client, _ := client.New(dockerVersion)
+	jobs, _ := queue.New(jobQueueName, queueAddr)
+	results, _ := queue.New(resultQueueName, queueAddr)
+	worker := New(jobs, results, client, os.Stdout)
+	// Test handleJob
+	worker.handleJob("test")
+	// Make sure correct stuff is put on result queue
+	select {
+	case <-results.Messages:
+		break
+	default:
+		t.Errorf("Failed to put data on result queue")
+	}
 }
