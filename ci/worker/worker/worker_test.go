b9dff9035a644c5683688b31ff61982006acf7ac
Harrison Turton <harrisonturton@gmail.com>
2018-10-27 19:50:56 +1100

    Make sure worker tests work with mocked queue and client services

--- ci/worker/worker/worker_test.go
+++ ci/worker/worker/worker_test.go
@@ -1,13 +1,22 @@
 package worker
 
 import (
+	"github.com/harrisonturton/submission-control/ci/mock/client"
+	"github.com/harrisonturton/submission-control/ci/mock/queue"
+	"os"
 	"testing"
 )
 
+const (
+	jobQueueName    = "job_queue"
+	resultQueueName = "result_queue"
+	queueAddr       = "amqp://guest:guest@localhost:5672/"
+	dockerVersion   = "1.38"
+)
+
 func TestNew(t *testing.T) {
-	expected := &Worker{
-		Jobs: &queue.Queue{
-				
-		}
-	}
+	client, _ := client.New(dockerVersion)
+	jobQueue, _ := queue.New(jobQueueName, queueAddr)
+	resultQueue, _ := queue.New(resultQueueName, queueAddr)
+	New(jobQueue, resultQueue, client, os.Stdout)
 }
