675abaabf016d898dba6a46b5fde910e5f0e9871
Harrison Turton <harrisonturton@gmail.com>
2018-10-29 15:20:35 +1100

    Add queue integration test

---
+++ ci/queue/integration_test.go
@@ -0,0 +1,40 @@
+package queue
+
+import (
+	"testing"
+	"time"
+)
+
+const (
+	queueName = "test_queue"
+	host      = "localhost"
+	queueAddr = "amqp://guest:guest@" + host + ":5672/"
+	message   = "test message"
+	timeout   = 5 * time.Second
+)
+
+func Test(t *testing.T) {
+	// Setup queue
+	queue, err := New(queueName, queueAddr)
+	if err != nil {
+		t.Fatalf("Failed with error: %s", err)
+	}
+
+	t.Run("Push and pop", func(t *testing.T) {
+		if err := queue.Push([]byte(message)); err != nil {
+			t.Fatalf("Failed with error: %s", err)
+		}
+		msgs := queue.Stream()
+		var response []byte
+		select {
+		case msg := <-msgs:
+			response = msg
+		case <-time.After(timeout):
+			t.Fatalf("Failed to pop message: timed out")
+		}
+		if string(response) != message {
+			t.Fatalf("Recieved unexpected message: %s", string(response))
+		}
+	})
+	queue.Close()
+}
