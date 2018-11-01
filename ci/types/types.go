9cf29580b650fe49a838bdf9ce50df0406a1d53a
Harrison Turton <harrisonturton@gmail.com>
2018-11-01 23:43:43 +1100

    Remove types.Compare, use reflect.DeepEqual instead

--- ci/types/types.go
+++ ci/types/types.go
@@ -23,35 +23,3 @@ type Environment struct {
 	Image *string            `json:"image"`
 	Vars  *map[string]string `json:"vars"`
 }
-
-// Compare determines if the two configurations
-// are the same.
-func (a TestConfig) Compare(b TestConfig) bool {
-	if a.Version != b.Version || *a.Version != *b.Version {
-		return false
-	}
-	if (a.Env == nil) != (b.Env == nil) {
-		return false
-	}
-	if a.Env == nil && b.Env == nil {
-		return true
-	}
-	return a.Env.Compare(*b.Env)
-}
-
-// Compare two environment configs
-func (a Environment) Compare(b Environment) bool {
-	if *a.Image != *b.Image {
-		return false
-	}
-	for keyA, valA := range *a.Vars {
-		valB, ok := (*b.Vars)[keyA]
-		if !ok {
-			return false
-		}
-		if valA != valB {
-			return false
-		}
-	}
-	return true
-}
