2f3f542fa5761c726c61b89bed3f3f9955ac0c5d
Harrison Turton <harrisonturton@gmail.com>
2018-12-27 11:38:37 +1100

    Add middleware to reject all requests that have an invalid JWT token

--- router/auth_route.go
+++ router/auth_route.go
@@ -6,87 +6,68 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
+	"io"
 	"net/http"
 	"time"
 )
-
-var jwtTimeout = time.Minute * 5
-
-var signingKey = []byte("asdgasdfglhjasbfjhkagsdlvgy gasdyfg")
-
-// Claims is the data passed with the JWT for
-// authentication
-type Claims struct {
-	Email string
-	jwt.StandardClaims
-}
-
-/*
-1) On client, ask for username and password
-2) Exchange the username and password for a time-limited access token via HTTPS. Use jwt-go on the server
-   to create the token. Use bcrypt to encrypt and compare passwords.
-3) Add the recieved access token to the request header for any RESTful API requiring authorization
-4) On the server, add an access token checker middleware for those routes. JWT tokens have an expire (exp)
-   and not before (nbf) timestamp. JWT validates those when it parses the token from the header.
-5) On client, periodically refresh the token. Our tokens expire in 5 minutes. I refresh them every 4 minutes
-*/
 
 func (router *Router) authHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		router.notFoundHandler(w, r)
 		return
 	}
-	var login LoginRequest
-	err := json.NewDecoder(r.Body).Decode(&login)
+	// Decode login request
+	login, err := decodeLoginRequest(r.Body)
 	if err != nil {
-		router.logger.Printf("Error decoding request body: %v\n", err)
+		router.logger.Printf("Error decoding login request: %v\n", err)
 		http.Error(w, "cannot decode body", http.StatusBadRequest)
-		return
 	}
-	router.logger.Printf("Email: %s // Password: %s\n", login.Email, login.Password)
+	// Get account from email
 	account, err := router.store.GetAccountByEmail(login.Email)
 	if err != nil {
-		router.logger.Printf("Cannot find user: %v\n", err)
+		router.logger.Printf("Cannot find user for email %s: %v\n", login.Email, err)
 		http.Error(w, "request failed", http.StatusBadRequest)
 		return
 	}
-	router.logger.Printf("Uid for %s is %s\n", account.Firstname, account.UID)
-	result, err := checkPassword(login.Password, account.Password)
-	if err != nil {
+	// Verify login information
+	ok, err := checkPassword(login.Password, account.Password)
+	if !ok || err != nil {
 		router.logger.Printf("Failed to check password: %v\n", err)
 		http.Error(w, "request failed", http.StatusBadRequest)
 		return
 	}
-	if result {
-		token, err := generateToken(account.Email)
-		if err != nil {
-			http.Error(w, "authentication failed", http.StatusBadRequest)
-			return
-		}
-		router.logger.Printf("Successfully authenticated %s (%s)\n", account.Firstname, account.UID)
-		fmt.Fprintf(w, token)
-	} else {
-		router.logger.Printf("Failed to authenticate %s (%s)\n", account.Firstname, account.UID)
-		fmt.Fprintf(w, "Failed to authenticate...")
-	}
-}
-
-func checkPassword(attempt string, hash string) (bool, error) {
-	bytes, err := hex.DecodeString(hash)
+	// Generate new token
+	token, err := generateToken(account.Email)
 	if err != nil {
-		return false, err
+		router.logger.Printf("Error generating token for %s: %v\n", account.Firstname, err)
+		http.Error(w, "request failed", http.StatusInternalServerError)
+		return
 	}
-	err = bcrypt.CompareHashAndPassword(bytes, []byte(attempt))
-	return err == nil, nil
+	fmt.Fprintf(w, token)
 }
 
 func generateToken(email string) (string, error) {
 	claims := Claims{
 		email,
 		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(jwtTimeout).Unix(),
+			ExpiresAt: time.Now().Add(TokenTimeout).Unix(),
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(signingKey)
+	return token.SignedString(SigningKey)
+}
+
+func decodeLoginRequest(body io.Reader) (*LoginRequest, error) {
+	var login LoginRequest
+	err := json.NewDecoder(body).Decode(&login)
+	return &login, err
+}
+
+func checkPassword(plaintextAttempt string, actualHash string) (bool, error) {
+	bytes, err := hex.DecodeString(actualHash)
+	if err != nil {
+		return false, err
+	}
+	err = bcrypt.CompareHashAndPassword(bytes, []byte(plaintextAttempt))
+	return err == nil, nil
 }
--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/harrisonturton/submission-control/store"
 	"log"
 	"net/http"
@@ -21,12 +22,42 @@ func NewRouter(logger *log.Logger, store *store.Store) *Router {
 	router := &Router{}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/auth", router.authHandler)
-	mux.HandleFunc("/users", router.usersHandler)
-	mux.HandleFunc("/", router.notFoundHandler)
+	mux.HandleFunc("/users", router.authMiddleware(router.usersHandler))
+	mux.HandleFunc("/", router.authMiddleware(router.notFoundHandler))
 	router.mux = mux
 	router.logger = logger
 	router.store = store
 	return router
+}
+
+func (router *Router) authMiddleware(nextHandler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenStr, ok := r.Header["Authorization"]
+		if !ok || len(tokenStr) == 0 {
+			http.Error(w, "authentication failure", http.StatusUnauthorized)
+			return
+		}
+		var claims Claims
+		token, err := jwt.ParseWithClaims(tokenStr[0], &claims, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				http.Error(w, "authentication failure", http.StatusUnauthorized)
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return SigningKey, nil
+		})
+		if err != nil {
+			router.logger.Println("claim: " + tokenStr[0])
+			router.logger.Printf("Failed to parse claims: %v\n", err)
+			http.Error(w, "authentication failure", http.StatusUnauthorized)
+			return
+		}
+		_, ok = token.Claims.(*Claims)
+		if !ok || !token.Valid {
+			http.Error(w, "authentication failure", http.StatusUnauthorized)
+			return
+		}
+		nextHandler(w, r)
+	}
 }
 
 // ServeHTTP will route the request to the handler with
