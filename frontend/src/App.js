7878343fee2d6ac0cae4b0a95c47ca676e372727
Harrison Turton <harrisonturton@gmail.com>
2019-01-03 22:42:15 +1100

    Add basic header components for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import search from "./search.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+		  <header>
+			  {/* Header Left */}
+			  <div>
+				  <h1>Concurrent & Distributed Systems</h1>
+			  </div>
+			  {/* Header Right */}
+			  <div>
+				  <h4>LOGOUT</h4>
+			  </div>
+		  </header>
+		  <div className="tab-bar">
+			  {/* Tab Left */}
+			  <div>
+				  <ul className="tabs">
+					  <li><a href="">Overview</a></li>
+					  <li><a href="">Participation</a></li>
+					  <li><a href="">Feedback</a></li>
+				  </ul>
+			  </div>
+			  {/* Tab Right */}
+			  <div>
+				  <div className="searchbar">
+					  <input type="text" className="searchbar-input"/>
+					  <img src={search} className="search-icon"/>
+				  </div>
+			  </div>
+		  </div>
       </div>
     );
   }
