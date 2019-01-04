5ed2cef104627bb3a43079e3242c713f835dcdbf
Harrison Turton <harrisonturton@gmail.com>
2019-01-04 14:53:04 +1100

    Arrange components into relevant directories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import search from "./search.png";
-import Header from "./Header.js";
+import React, { Component } from "react";
+import search from "./assets/search.png";
+import Header from "./common/Header.js";
 
 export default class App extends Component {
 	render() {
@@ -20,39 +19,3 @@ export default class App extends Component {
 		);
 	}
 }
-
-/*class App extends Component {
-  render() {
-    return (
-      <div className="app">
-		  <header>
-			  {/* Header Left /}
-			  <div>
-				  <h1>Concurrent & Distributed Systems</h1>
-			  </div>
-			  {/* Header Right /}
-			  <div>
-				  <h4>LOGOUT</h4>
-			  </div>
-		  </header>
-		  <div className="tab-bar">
-			  {/* Tab Left /}
-			  <div>
-				  <ul className="tabs">
-					  <li><a href="">Overview</a></li>
-					  <li><a href="">Participation</a></li>
-					  <li><a href="">Feedback</a></li>
-				  </ul>
-			  </div>
-			  {/* Tab Right /}
-			  <div>
-				  <div className="searchbar">
-					  <input type="text" className="searchbar-input"/>
-					  <img src={search} className="search-icon"/>
-				  </div>
-			  </div>
-		  </div>
-      </div>
-    );
-  }
-}*/
