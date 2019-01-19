e887b78720e400d06f60bccc3465661619db1708
Harrison Turton <harrisonturton@gmail.com>
2019-01-19 12:20:58 +1100

    Refactor HomeScreen to just redirect to a course page

--- frontend/src/containers/HomeScreen/index.js
+++ frontend/src/containers/HomeScreen/index.js
@@ -2,83 +2,19 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
-import "./style.css";
-import {
-	Header,
-	AssessmentItemList,
-	AssessmentFeedbackList,
-} from "components";
 
-class Home extends Component {
-	renderAssessment(assignments, labs) {
-		return (
-			<div className="assessment-wrapper">
-				<AssessmentItemList
-					title="Upcoming Assignments"
-					subtitle=""
-					items={assignments}
-				/>
-				<AssessmentItemList
-					title="Upcoming Labs"
-					subtitle=""
-					items={labs}
-				/>
-			</div>
-		);
-	}
-	renderFeedback(feedback) {
-		return (
-			<div className="feedback-wrapper">
-				<AssessmentFeedbackList
-					title="Assessment Feedback"
-					subtitle=""
-					submissions={feedback}
-				/>
-			</div>
-		);
-	}
-	render() {
-		let { is_authenticated, header, assignments, labs, feedback } = this.props;
-		if (!is_authenticated) {
-			return <Redirect to="/login"/>;
-		}
-		console.log("Rendering home...", JSON.stringify(labs));
-		return (
-			<div className="home-wrapper">
-				<Header
-					currentCourse={header.current_course}
-					courses={header.courses}
-				/>
-				{this.renderAssessment(assignments, labs)}
-				{this.renderFeedback(feedback)}
-			</div>
-		);
-	}
-}
+const Home = ({ courses }) => {
+	let course_id = courses[0].id;	
+	return <Redirect to={`/course/${course_id}`}/>
+};
 
 Home.propTypes = {
-	is_authenticated: PropTypes.bool.isRequired,
-	header:      PropTypes.object.isRequired,
-	assignments: PropTypes.array.isRequired,
-	labs:        PropTypes.array.isRequired,
-	feedback:    PropTypes.array.isRequired
+	courses: PropTypes.array.isRequired
 };
 
 const mapStateToProps = state => ({
-	is_authenticated: state.auth.is_authenticated,
-	header: mapHeaderState(state),
-	assignments: state.data.assessment.assignments,
-	labs: state.data.assessment.labs,
-	feedback: state.data.submissions === undefined ? [] : state.data.submissions
+	courses: state.data.courses
 });
-
-const mapHeaderState = state => {
-	let courses = state.data.courses;
-	return {
-		current_course: courses[0] === undefined ? "" : courses[0].name,
-		courses:        courses[0] === undefined ? [] : courses,
-	};
-};
 
 const HomeScreen = connect(
 	mapStateToProps,
