02c148378d1407dee5effe61ed0640ad221b66e1
Harrison Turton <harrisonturton@gmail.com>
2019-01-21 10:11:00 +1100

    Refactor AssessmentScreen to use WithHeader

--- frontend/src/containers/AssessmentScreen/index.js
+++ frontend/src/containers/AssessmentScreen/index.js
@@ -1,26 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Redirect } from "react-router-dom";
-import { Header } from "containers";
 import { connect } from "react-redux";
+import { WithHeader } from "containers";
 import { SubmissionList } from "components";
 import "./style.css";
 
-const _AssessmentScreen = ({ match, is_authenticated, courses, assessment, submissions }) => {
-	let { course_id, assessment_id } = match.params;
-	let filtered_submissions = submissions.filter(sub => sub.assessment_id == assessment_id);
-	let assessment_name = assessment.find(ass => ass.id == assessment_id).name;
-	if (!is_authenticated) {
-		return <Redirect to="/login"/>;
-	}
-	return (
-		<div className="assessment-screen">
-			<Header
-				currentCourse={courses.find(course => course.id == course_id).name}	
-				courses={courses.filter(course => course.id != course_id)}	
-			/>
-			<div className="left-wrapper">
-				<h1>{assessment_name}</h1>
+class _AssessmentScreen extends Component {
+	renderLeftColumn(assessmentName) {
+		return (
+			<div className="left-column-wrapper">
+				<h1>{assessmentName}</h1>
 				<p>You can make multiple submissions to a single assignment or lab. Each submission will be tested against the test suite, and you’ll see the result. Your tutors and convenors can see & give feedback on any submission you make.</p>
 				<form>
 					<label>Title</label>
@@ -40,29 +29,41 @@ const _AssessmentScreen = ({ match, is_authenticated, courses, assessment, submi
 					<input type="submit" value="Submit"/>
 				</form>
 			</div>
-			<div className="right-wrapper">
+		);
+	}
+	renderRightColumn(courseID, assessmentID, submissions) {
+		return (
+			<div className="right-column-wrapper">
 				<SubmissionList
 					title="Your Submissions"
 					subtitle=""
-					courseID={course_id}
-					assessmentID={assessment_id}
-					submissions={filtered_submissions}
+					courseID={courseID}
+					assessmentID={assessmentID}
+					submissions={submissions}
 				/>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+	render() {
+		var { match, assessment, submissions } = this.props;
+		let { course_id, assessment_id } = match.params;
+		var submissions = submissions.filter(sub => sub.assessment_id == assessment_id);
+		let assessmentName = assessment.find(ass => ass.id == assessment_id).name;
+		return (
+			<WithHeader className="assessment-screen" currentCourseID={course_id}>
+				{this.renderLeftColumn(assessmentName)}
+				{this.renderRightColumn(course_id, assessment_id, submissions)}
+			</WithHeader>
+		);
+	}
+}
 
 _AssessmentScreen.propTypes = {
-	is_authenticated: PropTypes.bool.isRequired,
-	courses:          PropTypes.array.isRequired,
 	assessment:       PropTypes.array.isRequired,
 	submissions:      PropTypes.array.isRequired,
 };
 
 const mapStateToProps = state => ({
-	is_authenticated: state.auth.is_authenticated,
-	courses: state.data.courses,
 	assessment: [ ...state.data.assessment.assignments, ...state.data.assessment.labs ],
 	submissions: state.data.submissions,
 });
