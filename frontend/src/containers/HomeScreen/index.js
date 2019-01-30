23249e50eefac7925e0a06c9329e3d893e68503e
Harrison Turton <harrisonturton@gmail.com>
2019-01-31 10:44:23 +1100

    Add redirect to login page for HomeScreen

--- frontend/src/containers/HomeScreen/index.js
+++ frontend/src/containers/HomeScreen/index.js
@@ -4,9 +4,12 @@ import PropTypes from "prop-types";
 import { Redirect } from "react-router-dom";
 import { Loader } from "components";
 
-const Home = ({ courses }) => {
+const Home = ({ isAuthenticated, courses }) => {
 	let has_loaded = courses.length > 0;
 	let course = courses[0];
+	if (!isAuthenticated) {
+		return <Redirect to="/login"/>;
+	}
 	if (!has_loaded || course.id == undefined) {
 		return (
 			<div style={{display: "flex", alignItems: "center", justifyContent: "center", width: "100vw", height: "100vh"}}>
@@ -18,11 +21,13 @@ const Home = ({ courses }) => {
 };
 
 Home.propTypes = {
-	courses: PropTypes.array.isRequired
+	courses:         PropTypes.array.isRequired,
+	isAuthenticated: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
-	courses: state.data.courses
+	isAuthenticated: state.auth.is_authenticated,
+	courses: state.data.courses,
 });
 
 const HomeScreen = connect(
