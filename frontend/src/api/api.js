55b8b58762097bd56d5dce09c98d68e168afb4f4
Harrison Turton <harrisonturton@gmail.com>
2019-01-31 16:34:20 +1100

    Refactor api calls and add API call to studentState endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,94 +5,78 @@ import * as routes from "api/routes";
 // uniquely identified by their email.
 // { uid, first_name, last_name, email }
 export const fetchUserData = async (email, token) => {
-	// Make sure request completes successfully
 	let url = `${routes.user}?email=${email}`;
-	let resp = await fetch(url, {
-		method: "GET",
-		headers: {
-			"Authorization": token	
-		}
-	});
-	if (!resp.ok) {
-		console.log(resp.statusText);
-		return null;	
-	}
-	// Check for data
-	let body = await resp.json();
-	if (body.user === null || body.user === undefined) {
-		console.log("body.user is null");
-		return null;
-	}
-	return body.user;
+	let body = await get(url, token, [
+		"user"
+	]);
+	return body === null ? null : body.user;
 };
 
 // fetchCourses returns a list of courses a user is enrolled in.
 // [ { id, name, course_code, period, year } ]
 export const fetchCourses = async (uid, token) => {
-	// Make sure request completes successfully
 	let url = `${routes.enrolled}?uid=${uid}`;
-	let resp = await fetch(url, {
-		method: "GET",
-		headers: {
-			"Authorization": token	
-		}
-	});
-	if (!resp.ok) {
-		console.log(resp.statusText);
-		return null;	
-	}
-	// Check for data
-	let body = await resp.json();
-	if (body.courses === null || body.courses === undefined) {
-		return null;	
-	}
-	return body.courses;
-}
+	let body = await get(url, token, [
+		"courses"	
+	]);
+	return body === null ? null : body.courses;
+};
 
-// fetchAssessment will return a list of assessments for a user.
 export const fetchAssessment = async (uid, token) => {
-	// Make sure request completes successfully
 	let url = `${routes.assessment}?uid=${uid}`;
-	let resp = await fetch(url, {
-		method: "GET",
-		headers: {
-			"Authorization": token
-		}
-	});
-	if (!resp.ok) {
-		console.log(resp.statusText);
-		return null;	
-	}
-	// Check for data
-	let body = await resp.json();
-	if (body.assessment === null || body.assessment == null) {
-		return null;	
-	}
-	return {
+	let body = await get(url, token, [
+		"assessment"	
+	]);
+	return body === null ? null : {
 		assignments: body.assessment.filter(item => item.type === "assignment"),
 		labs: body.assessment.filter(item => item.type === "lab"),
-	}
-}
+	};
+};
 
 // fetchSubmissions will return an array of all submissions the user
 // has ever made.
 export const fetchSubmissions = async (uid, token) => {
 	// Make sure request completes successfully
 	let url = `${routes.submissions}?uid=${uid}`;
+	let body = await get(url, token, [
+		"submissions"
+	]);
+	return body.submissions === null ? null : body.submissions;
+}
+
+export const fetchStudentState = async (email, token) => {
+	let url  = `${routes.studentState}?email=${email}`;
+	let body = await get(url, token, [
+		"enrolled",
+		"user",
+		"submissions",
+		"assessment"
+	]);
+	return {
+		user: body.user,
+		submissions: body.submissions,
+		courses: body.enrolled,
+		assessment: {
+			assignments: body.assessment.filter(item => item.type === "assignment"),
+			labs: body.assessment.filter(item => item.type === "lab"),
+		}
+	}
+};
+
+const get = async (url, token, responseKeys) => {
 	let resp = await fetch(url, {
 		method: "GET",
 		headers: {
-			"Authorization": token
+			"Authorization": token	
 		}
 	});
 	if (!resp.ok) {
-		console.log(resp.statusText);
-		return null;	
+		return null;
 	}
-	// Check for data
 	let body = await resp.json();
-	if (body.submissions === null || body.submissions == null) {
-		return null;	
-	}
-	return body.submissions;
+	return keysAreValid(body, responseKeys) ? body : null;
 }
+
+// Will check that every key has a value that is not null or undefined.
+const keysAreValid = (obj, keys) =>
+	Object.keys(obj).every(key => obj[key] !== null && obj[key] !== undefined);
--- frontend/src/api/routes.js
+++ frontend/src/api/routes.js
@@ -1,9 +1,10 @@
 
 // API endpoints
 const host = "http://localhost";
-export const auth        = host + "/auth";
-export const refresh     = host + "/refresh";
-export const enrolled    = host + "/enrol";
-export const assessment  = host + "/assessment";
-export const user        = host + "/user";
-export const submissions = host + "/submissions";
+export const auth         = host + "/auth";
+export const refresh      = host + "/refresh";
+export const enrolled     = host + "/enrol";
+export const assessment   = host + "/assessment";
+export const user         = host + "/user";
+export const submissions  = host + "/submissions";
+export const studentState = host + "/state/student";
